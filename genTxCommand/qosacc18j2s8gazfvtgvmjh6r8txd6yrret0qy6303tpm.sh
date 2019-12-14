9349b0cdbe6a88d0458d4c5fc6557d295cca28d7
snowdiceX <snowdice@outlook.com>
2019-12-14 12:10:50 +0800

    remove some sh

--- genTxCommand/qosacc18j2s8gazfvtgvmjh6r8txd6yrret0qy6303tpm.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-qosd gentx --compound --moniker "Bit Cat" --owner "qosacc18j2s8gazfvtgvmjh6r8txd6yrret0qy6303tpm" --tokens "3036976" --website "" --logo "http://validator-static.qoschain.info/public/logos/bitcat.png" --delegations "qosacc18j2s8gazfvtgvmjh6r8txd6yrret0qy6303tpm,3036976qos" 
--- genTxCommand/qosacc1mhdphazuaq54kwmftl5630acek8qyuwk5ezvgg.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-qosd gentx --compound --moniker "起源资本" --owner "qosacc1mhdphazuaq54kwmftl5630acek8qyuwk5ezvgg" --tokens "20000000" --website "" --logo "http://validator-static.qoschain.info/public/logos/qyzb.png" --delegations "qosacc1mhdphazuaq54kwmftl5630acek8qyuwk5ezvgg,20000000qos" 
